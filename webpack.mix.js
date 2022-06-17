cd63d3be2e03c0702b0141aeec5991ed336cb1a8
David Matthams <d.matthams@icloud.com>
2022-06-17 16:18:33 +0000

    reshuffle

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,19 +1,25 @@
 let mix = require('laravel-mix');
 
-mix.js('src/home.js', 'dist');
-mix.js('src/post.js', 'dist');
-mix.css('src/app.css', 'dist');
+// mix.js('src/home.js', 'dist');
+// mix.js('src/post.js', 'dist');
+// mix.css('src/app.css', 'dist');
 
 mix.browserSync({
 	proxy: 'https://dmatthams.webflow.io',
-	files: "./src/",
+	files: './js/',
 	serveStatic: ['.'],
-	// rewriteRules: [
-	//   {
-	// 	match: new RegExp('https://uploads-ssl.webflow.com/5f11a16f2e41f9b9380c2796/css/dmatthams.webflow.26d0ca22c.min.css'),
-	// 	fn: function() {
-	// 	  return '/app.css';
-	// 	}
-	//   }
-	// ]
+	rewriteRules: [
+		{
+			match: new RegExp('https://cdn.jsdelivr.net/gh/dmatthams/dmatthams-webflow/src/home.js'),
+			fn: function () {
+				return '/js/home.js';
+			},
+		},
+		{
+			match: new RegExp('https://cdn.jsdelivr.net/gh/dmatthams/dmatthams-webflow/src/post.js'),
+			fn: function () {
+				return '/js/post.js';
+			},
+		},
+	],
 });
